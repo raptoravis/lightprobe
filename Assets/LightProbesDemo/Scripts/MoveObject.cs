3795d8cad2d1dde96e27b3b8bf768b9131c8c39c
liyonggang <liyonggang@ztgame.com>
2021-02-03 17:09:17 +0800

    add  it

---
+++ Assets/LightProbesDemo/Scripts/MoveObject.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObject : MonoBehaviour
+{
+    Transform CubeTransform;
+    float speed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CubeTransform = GameObject.Find("Cube").transform; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            CubeTransform.Translate(CubeTransform.forward * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            CubeTransform.Translate(CubeTransform.forward * -1 * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            CubeTransform.Translate(CubeTransform.right * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            CubeTransform.Translate(CubeTransform.right * -1 * Time.deltaTime * speed);
+        }
+    }
+}
